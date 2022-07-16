59aec0b7232ab1afae6b38b80aedb73867fb1841
Michael S DuWors <46774296+MSDPhoenix@users.noreply.github.com>
2022-07-16 12:37:57 +0800

    finished layout of authors.html

--- flask_app/controllers/authors.py
+++ flask_app/controllers/authors.py
@@ -1,30 +1,31 @@
+from crypt import methods
 from flask_app import app
 from flask import render_template,redirect,request,session,flash
 from flask_app.models import book
 
-@app.route('authors')
+@app.route('/authors/')
 def authors():
-    pass
+    return render_template('authors.html')
 
-@app.route('authors/<int:author_id>/')
+@app.route('/authors/<int:author_id>/')
 def author(author_id):
     pass
 
 
-@app.route('authors/save/<int:author_id>/')
+@app.route('/authors/save/',methods=['POST'])
 def author_save():
     pass
 
 
-@app.route('authors/update/<int:author_id>/')
+@app.route('/authors/update/<int:author_id>/')
 def author_update():
     pass
 
-@app.route('authors/edit/<int:author_id>/')
+@app.route('/authors/edit/<int:author_id>/')
 def author_edit():
     pass
 
-@app.route('authors/delete/<int:author_id>/')
+@app.route('/authors/delete/<int:author_id>/')
 def author_delete():
     pass
 
